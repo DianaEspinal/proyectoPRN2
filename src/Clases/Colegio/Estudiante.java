306a35e1a08541d9dfcacfc96a34c33cccbfbae4
diani <diani@Diebonita>
2023-04-28 12:29:21 -0600

    Cree la clase persona con su codigo.

---
+++ src/Clases/Colegio/Estudiante.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.Colegio;
+
+/**
+ *
+ * @author Diana.
+ */
+public class Estudiante extends Persona {
+    private int codigoEstudiante;
+    private int gradoCursado;
+    private String estadoAprobacion;
+    private String nombreEncargado;
+
+    public Estudiante() {
+    }
+    
+    
+}
