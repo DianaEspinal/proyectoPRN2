725ac6866756cc658819b000d639e2e238a2a925
diani <diani@Diebonita>
2023-04-28 14:14:40 -0600

    Clase estudiante

--- src/Clases/Colegio/Estudiante.java
+++ src/Clases/Colegio/Estudiante.java
@@ -10,12 +10,90 @@ package Clases.Colegio;
  * @author Diana.
  */
 public class Estudiante extends Persona {
+     //Declaracion de atributos del clase estudiante.
     private int codigoEstudiante;
-    private int gradoCursado;
     private String estadoAprobacion;
     private String nombreEncargado;
 
+    // Declaracion del constructor por defecto.
     public Estudiante() {
+    }
+    //Declaracion de constructores con parametros.
+    public Estudiante(int codigoEstudiante, String estadoAprobacion, String nombreEncargado) {
+        this.codigoEstudiante = codigoEstudiante;
+        this.estadoAprobacion = estadoAprobacion;
+        this.nombreEncargado = nombreEncargado;
+    }
+
+    public Estudiante(int codigoEstudiante, String estadoAprobacion, String nombreEncargado, String nombres, String apellidos, String telefono, String direccion, String correoElectronico, int codigoPersona) {
+        super(nombres, apellidos, telefono, direccion, correoElectronico, codigoPersona);
+        this.codigoEstudiante = codigoEstudiante;
+        this.estadoAprobacion = estadoAprobacion;
+        this.nombreEncargado = nombreEncargado;
+    }
+     // Metodos get y set.
+    public int getCodigoEstudiante() {
+        return codigoEstudiante;
+    }
+
+    public void setCodigoEstudiante(int codigoEstudiante) {
+        this.codigoEstudiante = codigoEstudiante;
+    }
+
+    public String getEstadoAprobacion() {
+        return estadoAprobacion;
+    }
+
+    public void setEstadoAprobacion(String estadoAprobacion) {
+        this.estadoAprobacion = estadoAprobacion;
+    }
+
+    public String getNombreEncargado() {
+        return nombreEncargado;
+    }
+
+    public void setNombreEncargado(String nombreEncargado) {
+        this.nombreEncargado = nombreEncargado;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
     }
     
     
