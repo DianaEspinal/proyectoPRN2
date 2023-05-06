305348b9b42ebf080076cff5808ee82d5cd3cd8d
diani <diani@Diebonita>
2023-05-05 19:49:03 -0600

    Creacion de JPAnel Estudiante

--- src/prn215_grupo_4_1/PnlEstudiante.java
+++ src/prn215_grupo_4_1/PnlEstudiante.java
@@ -559,7 +559,7 @@ public class PnlEstudiante extends javax.swing.JPanel {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ffrmEstudiante().setVisible(true);
+                new PnlEstudiante().setVisible(true);
             }
         });
      }
