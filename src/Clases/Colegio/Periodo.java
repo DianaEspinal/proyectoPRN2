a82c927589d43bdb8798796c078658549fec84f5
diani <diani@Diebonita>
2023-05-08 12:19:26 -0600

    Acomodo

---
+++ src/Clases/Colegio/Periodo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases.Colegio;
+
+/**
+ *
+ * @author Admin
+ */
+public class Periodo {
+
+    public Integer getCodigoPeriodo() {
+        return codigoPeriodo;
+    }
+
+    public void setCodigoPeriodo(Integer codigoPeriodo) {
+        this.codigoPeriodo = codigoPeriodo;
+    }
+
+    public Integer getNumeroPeriodo() {
+        return numeroPeriodo;
+    }
+
+    public void setNumeroPeriodo(Integer numeroPeriodo) {
+        this.numeroPeriodo = numeroPeriodo;
+    }
+
+    public String getEstadoAprobacion() {
+        return estadoAprobacion;
+    }
+
+    public void setEstadoAprobacion(String estadoAprobacion) {
+        this.estadoAprobacion = estadoAprobacion;
+    }
+
+    public String getNotaAcumulada() {
+        return notaAcumulada;
+    }
+
+    public void setNotaAcumulada(String notaAcumulada) {
+        this.notaAcumulada = notaAcumulada;
+    }
+    private Integer codigoPeriodo;
+    private Integer numeroPeriodo;
+    private String estadoAprobacion;
+    private String notaAcumulada;
+    
+}
